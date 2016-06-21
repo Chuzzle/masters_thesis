d47a7395192085a5f155a7c797752195ac639a42
Hanna <hanna_autio@hotmail.com>
2016-06-21 11:36:20 +0200

    Removed .d and .o files, added .gitignore

--- pop_test.d
+++
@@ -1,2 +0,0 @@
-pop_test.o pop_test.d : pop_test.cc population.h illegal_event_exception.h pop_init.h \
- constants.h
--- population.d
+++
@@ -1,2 +0,0 @@
-population.o population.d : population.cc population.h illegal_event_exception.h \
- pop_init.h constants.h
