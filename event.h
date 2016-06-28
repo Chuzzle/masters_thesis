d13dd44898b9ffc910a8919635dd8b18000b4aca
Hanna <hanna_autio@hotmail.com>
2016-06-28 18:55:47 +0200

    Made a first (experimental) event-class.

--- event.h
+++ event.h
@@ -1,6 +1,6 @@
 #include <vector>
 
-#include "Population.h"
+#include "population.h"
 
 #ifndef EVENT_H
 #define EVENT_H
@@ -8,8 +8,8 @@
 class Event {
 public:
   explicit Event();
-  double virtual find_rate(double t, Population& pop);
   double virtual generate_time(double t, Population& pop);
+  double virtual execute_event(double t, Population& pop);
 }
 
 #endif
---
+++ event_newday.cc
@@ -0,0 +1,13 @@
+#include <cmath>
+
+#include "event.h"
+
+class Event_newday : Event {
+  double generate_time(double t, Population& pop) {
+    return ceil(t)-t;
+  }
+
+  void execute_event(double t, Population& pop) {
+    return;
+  }
+}
