5162b0374223aefd742d2b36f7a42b8c2a020922
Hanna <hanna_autio@hotmail.com>
2016-08-15 16:00:41 +0200

    Started work.

---
+++ analysis/plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+def main():
+    
+    data_file = open('~/Documents/exjobb/program/data/' + inp_str)
+
+if __name__ == '__main__':
+    main()
