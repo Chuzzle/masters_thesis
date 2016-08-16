59be0a4fd00cf04a3c4124605ad9c465f5b16ab1
Hanna <hanna_autio@hotmail.com>
2016-08-16 17:33:20 +0200

    Python script reads and stores the data and constants file

--- analysis/plot.py
+++ analysis/plot.py
@@ -1,10 +1,28 @@
 import numpy as np
 import sys
+import os.path
 import matplotlib.pyplot as plt
+import json
 
-def main():
-    
-    data_file = open('~/Documents/exjobb/program/data/' + inp_str)
+def main(argv):
+    file_loc = os.path.dirname(os.path.relpath(__file__))
+    with open(file_loc + '../constants_runspec.json') as constants_file:
+        constants = json.load(constants_file)
+    data_file = open(file_loc + '../data/' + argv[0])
+    data = read_file(data_file, constants)
+    print data
+
+
+def read_file(data_file, constants):
+    # Numpy multidimensional arrays are indexed as array[depth, rows, cols]
+    sim_data = np.zeros((constants['NUM_RUNS'], constants['NUM_DAYS'], constants['NUM_POPS']), dtype = np.int)
+    for k in xrange(constants['NUM_RUNS']):
+        for n in xrange(constants['NUM_DAYS']):
+            data_string = data_file.readline()
+            sim_data[k,n,:] = (data_string.split())[1:]
+    return sim_data
+
+
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
